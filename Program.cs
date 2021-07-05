7fcdada8182e73ac3ea7d3cdde253bc6cb6b34f0
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-05 16:09:44 +0530

    Employee Wage Problem

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1_EmpAttendance.EmpAttendance();
+            //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmp.PartTimeEmp();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_WageForMonth.WageForMonth();
+            //UC6_WageTillNumOfWorkingDays.WageTillNumOfWorkingDays();
+            //UC7_EmpWageUsingClassMethod.EmpWageUsingClassMethod();
+            //UC8_EmpWageForMultipleCompanies.computeEmpWage("DMart", 20, 2, 10);
+            //UC8_EmpWageForMultipleCompanies.computeEmpWage("Reliance", 10, 4, 20);
+
+            //#region for Uc9 Implementation
+
+            //EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            //EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            //dMart.computeEmpWage();
+            //Console.WriteLine(dMart.toString());
+            //reliance.computeEmpWage();
+            //Console.WriteLine(reliance.toString());
+
+            //#endregion
+
+
+            #region for UC10
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
